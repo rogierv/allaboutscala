0fbc6eab28bdf609e584d32ac461347889052eab
rogierv <rogierv@gmail.com>
2016-09-14 16:13:42 +0200

    Continued training

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "allaboutscala"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-    
+
+libraryDependencies ++= Seq (
+  "ch.qos.logback" % "logback-classic" % "1.1.7",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0"
+)
---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_10/HelloWorldWithScalaLogging.scala
@@ -0,0 +1,12 @@
+package com.allaboutscala.chapter.one.tutorial_10
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * Created by rogier.verkaik on 14-9-2016.
+  */
+object HelloWorldWithScalaLogging extends App with LazyLogging {
+
+  logger.info("Hello World from Scala Logging")
+
+}
